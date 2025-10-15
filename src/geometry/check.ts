9b8f3d01e478b7c99680a5fd80e8f74d292af4e9
dxdye <s-tbertr@haw-landshut.de>
2025-10-15 06:37:09 +0200

    test: is king checked

--- src/geometry/check.ts
+++ src/geometry/check.ts
@@ -1,63 +1,82 @@
-import { Board, Color, Position, Direction, BoardPieceMap, Figure } from './types.ts';
+import { Board, Color, Position, Direction, BoardPieceMap, Figure, Piece } from './types.ts';
 import { boardToPieceMap, positionToCoordinate, getPieceFromPieceMap } from './board.ts';
 import { coordinateToPosition } from './transform.ts';
-import { isNil, isNotNil } from './helper.ts';
+import { isIndexInBound, isNil, isNotNil } from './helper.ts';
 import { calculateMoveListForKing } from './move.ts';
 
+const isOpponentKing = (color: Color, piece: Piece | null): boolean =>
+  isNotNil(piece) && piece.figure === 'KING' && color !== piece.color;
+
 const isPawnDirectionCorrect = (direction: Direction, color: Color) =>
-  (color === 'white' && direction.row === 1) || (color === 'black' && direction.row === -1);
+  (color === 'black' && direction.row === 1) || (color === 'white' && direction.row === -1);
 
 const checkDirectionForCheck = (
   pieceMap: BoardPieceMap,
   kingPosition: Position | undefined,
   directions: readonly Direction[],
   color: Color,
-  targetFigures: Figure[],
+  targetFigures: readonly Figure[],
   siftBoard: boolean = true,
 ) => {
-  directions.map((direction) => {
-    let step = siftBoard ? 1 : 7;
-    while (step < 8 && isNotNil(kingPosition)) {
-      //maybe a better way instead of using typeguard
-      const stepTemp = siftBoard ? step : 1;
+  if (kingPosition === undefined) {
+    return false;
+  } else {
+    const current = positionToCoordinate(kingPosition);
+    const upperBound = siftBoard ? 8 : 2; //2 for knight
+    for (const direction of directions) {
+      let step = 1;
 
-      const updatedRow = positionToCoordinate(kingPosition)[0] + direction.row * stepTemp;
-      const updatedCol = positionToCoordinate(kingPosition)[1] + direction.col * stepTemp;
-      const piece = getPieceFromPieceMap(pieceMap, coordinateToPosition(updatedCol, updatedRow));
-      if (piece !== null) {
-        if (
-          //check for pawn attack
-          step === 1 &&
-          piece.figure === 'PAWN' &&
-          targetFigures.includes('BISHOP') &&
-          piece.color !== color &&
-          isPawnDirectionCorrect(direction, piece.color)
-        ) {
-          return true;
-        }
+      while (step < upperBound) {
+        //maybe a better way instead of using typeguard
+        const stepTemp = siftBoard ? step : 1;
 
-        if (piece.color !== color && targetFigures.includes(piece.figure)) {
-          return true;
+        const updatedRow = current[0] + direction.row * stepTemp;
+        const updatedCol = current[1] + direction.col * stepTemp;
+        const isInBound = isIndexInBound(updatedRow) && isIndexInBound(updatedCol);
+        if (isInBound) {
+          const piece = getPieceFromPieceMap(pieceMap, coordinateToPosition(updatedCol, updatedRow));
+          if (piece !== null) {
+            if (
+              //check for pawn attack
+              piece.color !== color &&
+              step === 1 //only 1 step away
+            ) {
+              if (
+                piece.figure === 'PAWN' &&
+                targetFigures.includes('BISHOP') &&
+                isPawnDirectionCorrect(direction, piece.color)
+              ) {
+                return true;
+              }
+            }
+
+            const isTargetPiece = piece.color !== color && targetFigures.includes(piece.figure);
+            if (isTargetPiece) {
+              //f.ex. bishop or queen in diagonal direction
+              return true;
+            }
+
+            if (isOpponentKing(color, piece) && step === 1 && !targetFigures.includes('KNIGHT')) {
+              return true; //king is next to king
+              //hypothetical king check
+            }
+
+            if (piece.color === color) {
+              return false;
+            }
+            break; //blocked by own piece or opponent piece
+          }
         }
-        break; //blocked by own piece or opponent piece
+        //found piece in this direction
+        ++step;
       }
-      //found piece in this direction
-      ++step;
     }
-  });
+  }
   return false;
 };
 
-const isPositionChecked = (position: Position, board: Board) => {};
-
-const findKingPosition = (board: Board, color: Color): Position | undefined => {
-  const kingSquare = board.find((square) => square.figure === 'KING' && square.color === color);
-  return kingSquare ? { row: kingSquare.row, column: kingSquare.column } : undefined;
-};
-
-export const isKingChecked = (board: Board, color: Color): boolean => {
+export const isPositionChecked = (kingPosition: Position, board: Board, color: Color) => {
   const pieceMap = boardToPieceMap(board);
-  const kingPosition = findKingPosition(board, color);
   if (isNil(kingPosition)) {
     throw new Error('King not found on the board');
   } else {
@@ -90,8 +109,23 @@ export const isKingChecked = (board: Board, color: Color): boolean => {
     if (checkDirectionForCheck(pieceMap, kingPosition, bishopDirection, color, ['BISHOP', 'QUEEN'])) return true;
     //check by rook or queen
     if (checkDirectionForCheck(pieceMap, kingPosition, rookDirection, color, ['ROOK', 'QUEEN'])) return true;
+    //check by knight
     if (checkDirectionForCheck(pieceMap, kingPosition, knightMoves, color, ['KNIGHT'], false)) return true;
     return false;
+  }
+};
+
+const findKingPosition = (board: Board, color: Color): Position | undefined => {
+  const kingSquare = board.find((square) => square.figure === 'KING' && square.color === color);
+  return kingSquare ? { row: kingSquare.row, column: kingSquare.column } : undefined;
+};
+
+export const isKingChecked = (board: Board, kingColor: Color): boolean => {
+  const kingPosition = findKingPosition(board, kingColor);
+  if (kingPosition === undefined) {
+    throw new Error('King not found on the board');
+  } else {
+    return isPositionChecked(kingPosition, board, kingColor);
   }
 };
 
