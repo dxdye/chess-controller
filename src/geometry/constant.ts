171ed40a90a8cb325b2bcd10713ece458123f401
dxdye <s-tbertr@haw-landshut.de>
2025-10-29 19:36:42 +0100

    fix: castling (bad implementation of castling rights)

--- src/geometry/constant.ts
+++ src/geometry/constant.ts
@@ -1,7 +1,13 @@
 import type { Fen } from './types.ts';
+import { CastlingLetter } from './types.ts';
 
 export const WHITE_EN_PASSENT_ROW = 5;
 export const BLACK_EN_PASSENT_ROW = 4;
 export const SECOND_ROW = 2;
 export const SEVENTH_ROW = 7;
 export const INIT_POSITION: Fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+export const BOTH_CAN_CASTLE: CastlingLetter[] = ['K', 'Q', 'k', 'q'];
+export const WHITE_CAN_CASTLE: CastlingLetter[] = ['K', 'Q'];
+export const BLACK_CAN_CASTLE: CastlingLetter[] = ['k', 'q'];
+export const NO_CASTLING: CastlingLetter[] = [];
